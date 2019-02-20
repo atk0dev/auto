6eef0b4ddd03fd72544ab27617b1cbfda0f56059
Andrii Tkach <atk0@outlook.com>
2019-02-21 00:56:08 +0200

    adding vehicles api

--- src/AutoApp/AutoApp/Controllers/VehiclesController.cs
+++ src/AutoApp/AutoApp/Controllers/VehiclesController.cs
@@ -1,15 +1,48 @@
 using Microsoft.AspNetCore.Mvc;
 using AutoApp.Models;
+using AutoApp.Controllers.Resources;
+using AutoMapper;
+using AutoApp.Persistence;
+using System.Threading.Tasks;
+using System;
 
 namespace AutoApp.Controllers
 {
     [Route("/api/vehicles")]
     public class VehiclesController : Controller
     {
-        [HttpPost]
-        public IActionResult CreateVehicle([FromBody]Vehicle vehicle)
+
+        private readonly IMapper mapper;
+        private readonly AutoDbContext context;
+
+        public VehiclesController(IMapper mapper, AutoDbContext context)
         {
-            return Ok(vehicle);
+            this.context = context;
+            this.mapper = mapper;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateVehicle([FromBody]VehicleResource vehicleResource)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var model = await this.context.Models.FindAsync(vehicleResource.ModelId);
+            if (model == null)
+            {
+                ModelState.AddModelError("ModelId", "Invalid model id");
+                return BadRequest(ModelState);
+            }
+
+            var vehicle = this.mapper.Map<VehicleResource, Vehicle>(vehicleResource);
+            vehicle.LastUpdate = DateTime.Now;
+            this.context.Vehicles.Add(vehicle);
+            await this.context.SaveChangesAsync();
+
+            var result = mapper.Map<Vehicle, VehicleResource>(vehicle);
+            return Ok(result);
         }
     }
 }
--- src/AutoApp/AutoApp/Persistence/AutoDbContext.cs
+++ src/AutoApp/AutoApp/Persistence/AutoDbContext.cs
@@ -7,6 +7,9 @@ namespace AutoApp.Persistence
     {
         public DbSet<Make> Makes { get; set; }
         public DbSet<Feature> Features { get; set; }
+        public DbSet<Vehicle> Vehicles {get; set;}
+
+        public DbSet<Model> Models {get; set;}
 
         public AutoDbContext(DbContextOptions<AutoDbContext> options) : base(options)
         {            
